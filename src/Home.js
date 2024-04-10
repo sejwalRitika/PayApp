2109bc866054aed15829cfa82a9be7945ed81732
sejwalRitika <ritikasejwal330@gmail.com>
2024-04-10 15:58:33 +0530

    pages Update

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Aboutus from "./Aboutus";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Header from "./Header";
+import Homeitwork from "./Homeitwork";
+import Navbar from "./Navbar";
+import Service from "./Service";
+const Home = () =>{
+return(
+<>
+<Navbar/>
+<Header/>
+<Homeitwork/>
+<Aboutus/>
+<Service/>
+<Contact/>
+<Footer/>
+</>
+)
+}
+export default Home;
